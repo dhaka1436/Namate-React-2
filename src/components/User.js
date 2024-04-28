1fd0ad4cbcc2226014e617b83ac60b427f2db0bc
dhaka1436 <himanshudhaka987@gmail.com>
2024-04-29 02:34:46 +0530

    .

--- src/components/User.js
+++ src/components/User.js
@@ -15,7 +15,7 @@ const User = (props) => {
 
         <div className="user-card">
             
-            <h2> Name : Himanshu </h2>
+            <h2> Name : Himanshu Anoushka</h2>
             <h3> Location : Chandigarh </h3>
             <h4> Email : himanshudhaka987@gmail.com</h4>
             <h4> Contact No. : +91 7973537221 </h4>
